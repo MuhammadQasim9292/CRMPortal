ebd2fc187b5a22277c150021c0954b552fc26b52
Hania Arshad <144407628+HANIAARSHAD890@users.noreply.github.com>
2024-08-01 16:20:40 +0500

    latestpush

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2b0746a16b72c266fbad29eae97b3a0bdb6880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21881bf2f408772deb9f9b0648afaf90a2b7c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
