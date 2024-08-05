5d1ac1394dcbc6cc679de11a1213a74e12d14f68
muskan-912 <muskan.wordsense2.0@gmail.com>
2024-08-04 23:55:25 -0700

    created

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5384410f1e7d6124360340e929cc3af7689a381c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833785251d03b1c6860fd67db24bc6b3dfb2ea8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
